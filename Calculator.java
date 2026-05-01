533b30734f49d21aafffc056eef1612462b19f87
hajarnm <ihajarnm5@gmail.com>
2026-05-01 08:41:25 +0300

    Clean and stailize main calcutor version (v1.0)

--- Calculator.java
+++ Calculator.java
@@ -4,31 +4,38 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("=== Simple Calculator ===");
+
         System.out.println("Enter first number:");
         double a = input.nextDouble();
 
         System.out.println("Enter second number:");
         double b = input.nextDouble();
 
-
-
-        System.out.println("Choose operation (* /):");
+        System.out.println("Choose operation (+ - * /):");
         char op = input.next().charAt(0);
 
-        double result = 0;
+        double result;
 
         switch(op) {
+            case '+':
+                result = a + b;
+                break;
+
+            case '-':
+                result = a - b;
+                break;
+
             case '*':
                 result = a * b;
                 break;
 
             case '/':
-                if (b != 0)
-                    result = a / b;
-                else {
+                if (b == 0) {
                     System.out.println("Error: Cannot divide by zero");
                     return;
                 }
+                result = a / b;
                 break;
 
             default:
@@ -38,6 +45,6 @@ public class Calculator {
 
         System.out.println("Result: " + result);
 
-       
+        input.close();
     }
 }
