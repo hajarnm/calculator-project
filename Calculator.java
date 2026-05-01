1e81d91fe9a1d18b85a884c076fe0f4798dab80b
hajarnm <ihajarnm5@gmail.com>
2026-05-01 09:01:29 +0300

    Implement basic calculator with numeric menu system

--- Calculator.java
+++ Calculator.java
@@ -12,25 +12,31 @@ public class Calculator {
         System.out.println("Enter second number:");
         double b = input.nextDouble();
 
-        System.out.println("Choose operation (+ - * /):");
+
+        System.out.println("Choose operation:");
+        System.out.println("1 - Addition (+)");
+        System.out.println("2 - Subtraction (-)");
+        System.out.println("3 - Multiplication (*)");
+        System.out.println("4 - Division (/)");
+
         char op = input.next().charAt(0);
 
         double result;
 
         switch(op) {
-            case '+':
+            case '1':
                 result = a + b;
                 break;
 
-            case '-':
+            case '2':
                 result = a - b;
                 break;
 
-            case '*':
+            case '3':
                 result = a * b;
                 break;
 
-            case '/':
+            case '4':
                 if (b == 0) {
                     System.out.println("Error: Cannot divide by zero");
                     return;
